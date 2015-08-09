99785e54ee3e052f219b5bacf1e637fee8a412f4
Sam <samsface@gmail.com>
2015-08-09 01:41:15 +0100

    AHRS now just returns a rotation matrix

--- drone/Examples/AHRS/AHRS.cpp
+++ drone/Examples/AHRS/AHRS.cpp
@@ -1,36 +1,13 @@
 /*
-This code is provided under the BSD license.
-Copyright (c) 2014, Emlid Limited. All rights reserved.
-Written by Igor Vereninov and Mikhail Avkhimenia
-twitter.com/emlidtech || www.emlid.com || info@emlid.com
-
-Application: Mahory AHRS algorithm supplied with data from MPU9250.
-Outputs roll, pitch and yaw in the console and sends quaternion
-over the network - it can be used with 3D IMU visualizer located in
-Navio/Applications/3D IMU visualizer.
-
-To run this app navigate to the directory containing it and run following commands:
-make
-sudo ./AHRS
-
-If you want to visualize IMU data on another machine pass it's address and port
-sudo ./AHRS ipaddress portnumber
-
 To achieve stable loop you need to run this application with a high priority
 on a linux kernel with real-time patch. Raspbian distribution with real-time
 kernel is available at emlid.com and priority can be set with chrt command:
 chrt -f -p 99 PID
 */
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <sys/time.h>
 #include "Navio/MPU9250.h"
 #include "AHRS.hpp"
+#include <iostream>
 #include <vector>
 #include <thread>
 #include <chrono>
@@ -50,7 +27,7 @@ class gyro
       std::vector<float> offset(3);
       for(int i = 0; i< 100; i++) {
          _imu.getMotion6(&_accel[0], &_accel[1], &_accel[2], 
-                         &_gyro[0], &_gyro[1], &_gyro[2]);
+                         &_gyro[0],  &_gyro[1],  &_gyro[2]);
 	 offset[0] -= _gyro[0]*0.0175;
          offset[1] -= _gyro[1]*0.0175;
          offset[2] -= _gyro[2]*0.0175;
@@ -66,18 +43,27 @@ public:
       return i;
    }
 
-   void imuLoop() {  
+   std::vector<std::vector<float>> rotation() {  
       _t0 = _t1;
       _t1 = std::chrono::high_resolution_clock::now();
       auto dt = std::chrono::duration_cast<std::chrono::microseconds>(_t1-_t0).count();
-      if(dt < 1/1300.0) 
-         return;
-
+        
       _imu.getMotion6(&_accel[0], &_accel[1], &_accel[2], 
-                      &_gyro[0], &_gyro[1], &_gyro[2]);
-      _ahrs.updateIMU(_accel[0], _accel[1], _accel[2], 
+                      &_gyro[0],  &_gyro[1],  &_gyro[2]);
+
+      _ahrs.updateIMU(_accel[0],       _accel[1],       _accel[2], 
                       _gyro[0]*0.0175, _gyro[1]*0.0175, _gyro[2]*0.0175, dt);
 
+      float x = _ahrs.getX();
+      float y = _ahrs.getY();
+      float z = _ahrs.getZ();
+      float w = _ahrs.getW();
+
+      #define scf(x) static_cast<float>(x)
+      return {{ scf(1.0-2.0*(y*y+z*z)),  scf(2.0*(x*y+w*z)),      scf(2.0*(x*z-w*y))     },
+              { scf(2.0*(x*y-w*z)),      scf(1.0-2.0*(x*x+z*z)),  scf(2.0*(y*z+w*x))     },
+              { scf(2.0*(x*z+w*y)),      scf(2.0*(y*z-w*x)),      scf(1.0-2.0*(x*x+y*y)) }};
+      #undef scf
       // Soft and hard iron calibration required for proper function.
       // imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
       // ahrs.update(ax, ay, az, gx*0.0175, gy*0.0175, gz*0.0175, my, mx, -mz, dt);
@@ -88,4 +74,8 @@ public:
 
 int main(int argc, char *argv[]) {
     gyro& g = gyro::instance();
+    while(true) {
+       auto r = g.rotation();
+       std::cout << r[0][0] << std::endl;
+    }
 }
